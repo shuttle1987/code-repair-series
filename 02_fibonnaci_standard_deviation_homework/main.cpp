e5f2f97586af77c2aa0df208d82707905402080b
Janis Lesinskis <janis@lesinskis.com>
2014-07-19 23:39:19 -0400

    Cleaned up many aspects of the code. Brought the code to a state where the project specification is satisfied.

--- 02_fibonnaci_standard_deviation_homework/main.cpp
+++ 02_fibonnaci_standard_deviation_homework/main.cpp
@@ -1,22 +1,22 @@
 #include <iostream>
 #include <cmath>
 
-const int SIZE_OF_GENERIC_ARRAY = 100;
-const int SIZE_OF_FIBONACCI_ARRAY = 20;
+const size_t SIZE_OF_GENERIC_ARRAY = 100;
+const size_t SIZE_OF_FIBONACCI_ARRAY = 20;
 
-void fillGenericArray(float[], size_t);
+void fillSequentialIntegersArray(float[], size_t);
 void fillFibonacciArray(int[], size_t);
 
-double mean(float[], int);
-double mean(int[], int);
+double mean(float[], size_t);
+double mean(int[], size_t);
 
-double standardDeviation(float[], int);
-double standardDeviation(int[], int);
+double standardDeviation(float[], size_t);
+double standardDeviation(int[], size_t);
 
 void outputMean(float[], size_t);
-void outputStandardDeviation(float[], size_t);
-
 void outputMean(int[], size_t);
+
+void outputStandardDeviation(float[], size_t);
 void outputStandardDeviation(int[], size_t);
 
 int main(int argc, char* argv[])
@@ -33,26 +33,25 @@ int main(int argc, char* argv[])
 
    if (sequenceType == 'G' || sequenceType == 'g')
    {
-       fillGenericArray(SequentialNumbers, SIZE_OF_GENERIC_ARRAY);
+       fillSequentialIntegersArray(SequentialNumbers, SIZE_OF_GENERIC_ARRAY);
        outputMean(SequentialNumbers, SIZE_OF_GENERIC_ARRAY);
        outputStandardDeviation(SequentialNumbers, SIZE_OF_GENERIC_ARRAY);
    }
-
    else if (sequenceType == 'F' || sequenceType == 'f')
    {
        fillFibonacciArray(FibonacciNumbers, SIZE_OF_FIBONACCI_ARRAY);
        outputMean(FibonacciNumbers, SIZE_OF_FIBONACCI_ARRAY);
        outputStandardDeviation(FibonacciNumbers, SIZE_OF_FIBONACCI_ARRAY);
    }
-
    else
+   {
        std::cout << "\n"
             << "Invalid input. Please type 'F' or 'G'. Thank you.";
-
+   }
    return 0;
 }
 
-void fillGenericArray(float array_to_fill[], size_t array_size)
+void fillSequentialIntegersArray(float array_to_fill[], size_t array_size)
 {
    for (size_t i = 0; i < array_size; i++){
        array_to_fill[i] = i + 1;
@@ -69,57 +68,58 @@ void fillFibonacciArray(int array_to_fill[], size_t array_size)
    }
 }
 
-double mean(float Array[], int SIZE_OF_GENERIC_ARRAY)
+double mean(float Array[], size_t array_size)
 {
    double sumOfElements = 0;
-   int i;
 
-   for (i = 0; i < SIZE_OF_GENERIC_ARRAY; i++)
+   for (size_t i = 0; i < array_size; i++)
    {
        sumOfElements += Array[i];
    }
-   return sumOfElements / i;
+   return sumOfElements / array_size;
 }
 
-double mean(int Array[], int SIZE_OF_FIBONACCI_ARRAY)
+double mean(int Array[], size_t array_size)
 {
    double sumOfElements = 0;
-   int i;
 
-   for (i = 0; i < SIZE_OF_FIBONACCI_ARRAY; i++)
+   for (size_t i = 0; i < array_size; i++)
    {
        sumOfElements += Array[i];
    }
-   return sumOfElements / i;
+   return sumOfElements / array_size;
 }
 
-double standardDeviation(float Array[], int SIZE_OF_GENERIC_ARRAY)
+double standardDeviation(float Array[], size_t array_size)
 {
-   double standardDeviation;
    double tempSum = 0;
 
-   for (int i = 0; i < SIZE_OF_GENERIC_ARRAY; i++)
+   for (size_t i = 0; i < array_size; i++)
    {
-       tempSum += pow((Array[i] - mean(Array, SIZE_OF_GENERIC_ARRAY)), 2);
+       tempSum += pow((Array[i] - mean(Array, array_size)), 2);
    }
-   standardDeviation = sqrt(tempSum / (SIZE_OF_GENERIC_ARRAY));
-   return (standardDeviation);
+   return sqrt(tempSum / array_size);
 }
 
-double standardDeviation(int Array[], int SIZE_OF_FIBONACCI_ARRAY)
+double standardDeviation(int Array[], size_t array_size)
 {
-   double standardDeviation;
    double tempSum = 0;
 
-   for (int i = 0; i < SIZE_OF_FIBONACCI_ARRAY; i++)
+   for (size_t i = 0; i < array_size; i++)
    {
-       tempSum += pow((Array[i] - mean(Array, SIZE_OF_FIBONACCI_ARRAY)), 2);
+       tempSum += pow((Array[i] - mean(Array, array_size)), 2);
    }
-   standardDeviation = sqrt(tempSum / (SIZE_OF_FIBONACCI_ARRAY));
-   return (standardDeviation);
+   return sqrt(tempSum / array_size);
 }
 
 void outputMean(float array_to_display[], size_t array_size)
+{
+   std::cout << "\n";
+   std::cout << "The mean is: " << mean(array_to_display, array_size);
+   std::cout << std::endl;
+}
+
+void outputMean(int array_to_display[], size_t array_size)
 {
    std::cout << "\n";
    std::cout << "The mean is: " << mean(array_to_display, array_size);
@@ -133,18 +133,9 @@ void outputStandardDeviation(float array_to_display[], size_t array_size)
    std::cout << std::endl;
 }
 
-void outputMean(int array_to_display[], size_t array_size)
-{
-   std::cout << "\n";
-   std::cout << "The mean is: " << mean(array_to_display, array_size);
-   std::cout << std::endl;
-}
-
 void outputStandardDeviation(int array_to_display[], size_t array_size)
 {
    std::cout << "\n";
    std::cout << "The standard deviation is: " << standardDeviation(array_to_display, array_size);
    std::cout << std::endl;
 }
-
-
